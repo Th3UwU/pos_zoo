f7e905c0f515645bfbff0b740d910ac9c5ae919e
Th3UwU <angel.storres@alumnos.udg.mx>
2023-04-04 19:25:43 -0600

    am supplier

--- src/gui/am_product.ts
+++ src/gui/am_product.ts
@@ -1,6 +1,7 @@
 import { getCurrentWindow, dialog, BrowserWindow, getGlobal } from '@electron/remote';
 import { OpenDialogOptions } from 'electron';
 import { readFileSync } from 'fs';
+import { alertMessage } from '../misc';
 import Main from '../main';
 import Window from '../window';
 
@@ -31,24 +32,47 @@ buttonSupplier.addEventListener('click', () => {
 	main.createWindow(800, 600, 'gui/selectSupplier.html', getCurrentWindow());
 });
 
-const dialogOpenOptions: OpenDialogOptions = {title: 'Elegir imagen', properties: ['openFile']};
-
-let inputImage = document.getElementById('image') as HTMLInputElement;
+// Image
+let imagePath = document.getElementById('image') as HTMLInputElement;
 let buttonImage = document.getElementById('buttonImage') as HTMLButtonElement;
 let imagePreview = document.getElementById('imagePreview') as HTMLImageElement;
+
+const dialogOpenOptions: OpenDialogOptions = {title: 'Elegir imagen', properties: ['openFile']};
 
 buttonImage.addEventListener('click', () => {
 	let dialogResult: string[] = dialog.showOpenDialogSync(getCurrentWindow(), dialogOpenOptions);
 
 	if (dialogResult != undefined)
 	{
-		let imagePath: string = dialogResult[0];
-		inputImage.value = imagePath;
-		
-		imagePreview.src = imagePath;
-		imagePreview.style.display = 'block';
+		imagePath.value = dialogResult[0];
 
-		let imageRaw: Buffer = readFileSync(imagePath, null);
-		console.log(imageRaw[4]);
+		imagePreview.src = dialogResult[0];
+		imagePreview.style.display = 'block';
 	}
+});
+
+// Add new product
+let name = document.getElementById('name') as HTMLInputElement;
+let description = document.getElementById('description') as HTMLInputElement;
+let price = document.getElementById('price') as HTMLInputElement;
+let category = document.getElementById('category') as HTMLInputElement;
+let stock = document.getElementById('stock') as HTMLInputElement;
+let maxStock = document.getElementById('maxStock') as HTMLInputElement;
+let localLimit = document.getElementById('localLimit') as HTMLInputElement;
+let supplier = document.getElementById('supplier') as HTMLInputElement;
+
+let buttonAccept = document.getElementById('buttonAccept') as HTMLButtonElement;
+buttonAccept.addEventListener('click', async (): Promise<void> => {
+
+	try {
+		let imageRaw: string = readFileSync(imagePath.value, null).toString('base64');
+		let query: string = `INSERT INTO PRODUCT VALUES((SELECT MAX(ID_PRODUCT) FROM PRODUCT) + 1, '${supplier.value}', '${name.value}', '${description.value}', '${price.value}', '${category.value}', '${stock.value}', '${maxStock.value}', '${localLimit.value}', DEFAULT, (DECODE('${imageRaw}', 'base64')));`;
+		console.log(query);
+		await main.querySQL(query);
+
+	} catch (error: any) {
+		console.log(error);
+		alertMessage(getCurrentWindow(), {title: "Error", message: error.message, type: "error"});
+	}
+
 });
---
+++ src/gui/am_supplier.ts
@@ -0,0 +1,48 @@
+import { getCurrentWindow, dialog, BrowserWindow, getGlobal } from '@electron/remote';
+import { OpenDialogOptions } from 'electron';
+import { readFileSync } from 'fs';
+import { alertMessage } from '../misc';
+import Main from '../main';
+
+let main: Main = getGlobal('main');
+
+let name = document.getElementById('name') as HTMLInputElement;
+let address = document.getElementById('address') as HTMLInputElement;
+let tel = document.getElementById('tel') as HTMLInputElement;
+//let status = document.getElementById('status') as HTMLInputElement;
+
+// Image
+let imagePath = document.getElementById('image') as HTMLInputElement;
+let buttonImage = document.getElementById('buttonImage') as HTMLButtonElement;
+let imagePreview = document.getElementById('imagePreview') as HTMLImageElement;
+
+const dialogOpenOptions: OpenDialogOptions = {title: 'Elegir imagen', properties: ['openFile']};
+
+buttonImage.addEventListener('click', () => {
+	let dialogResult: string[] = dialog.showOpenDialogSync(getCurrentWindow(), dialogOpenOptions);
+
+	if (dialogResult != undefined)
+	{
+		imagePath.value = dialogResult[0];
+
+		imagePreview.src = dialogResult[0];
+		imagePreview.style.display = 'block';
+	}
+});
+
+// Add new supplier
+let buttonAccept = document.getElementById('buttonAccept') as HTMLButtonElement;
+buttonAccept.addEventListener('click', async (): Promise<void> => {
+
+	try {
+		let imageRaw: string = readFileSync(imagePath.value, null).toString('base64');
+		let query: string = `INSERT INTO SUPPLIER VALUES((SELECT MAX(ID_SUPPLIER) FROM SUPPLIER) + 1, '${name.value}', '${address.value}', ${tel.value}, (DECODE('${imageRaw}', 'base64')), DEFAULT);`;
+		console.log(query);
+		await main.querySQL(query);
+
+	} catch (error: any) {
+		console.log(error);
+		alertMessage(getCurrentWindow(), {title: "Error", message: error.message, type: "error"});
+	}
+
+});
