fc4ab1e30a74fa8c1a1df1939990b6cc14007839
Th3UwU <angel.storres@alumnos.udg.mx>
2023-04-23 13:13:51 -0600

    compra

--- src/gui/am_purchase.ts
+++ src/gui/am_purchase.ts
@@ -144,32 +144,13 @@ async function MAIN(): Promise<void>
 
 		// Accept button
 		button_accept.addEventListener('click', async (): Promise<void> => {
-
-			// Check at least 1 added element
-			let current_products_items = document.getElementsByClassName('product_item') as HTMLCollectionOf<HTMLDivElement>;
-			if (current_products_items.length == 0)
-			{
-				dialog.showMessageBoxSync(getCurrentWindow(), {title: "Error", message: "Ingrese por lo menos 1 producto a la compra", type: "error"});
-				return;
-			}
-
-			// Verificar que no rebase el stock maximo
-			for (const pi of current_products_items)
-			{
-				let product_info: any = (await main.querySQL(`SELECT MAX_STOCK, STOCK FROM PRODUCT WHERE ID_PRODUCT = ${parseInt(pi.dataset.id)};`)).rows[0];
-				let current_stock: number = parseInt(product_info.stock);
-				let max_stock: number = parseInt(product_info.max_stock);
-				if ((current_stock + (pi.querySelector('.amount') as HTMLInputElement).valueAsNumber) > max_stock)
-				{
-					dialog.showMessageBoxSync(getCurrentWindow(), {title: "Error", message: `La cantidad del producto '${pi.querySelector('.name').innerHTML}' excede el stock maximo (${max_stock})`, type: "error"});
-					return;
-				}
-			}
-
 			try
 			{
+				await await validateinputs();
+				let current_products_items = document.getElementsByClassName('product_item') as HTMLCollectionOf<HTMLDivElement>;
+
 				// DB Query
-				await main.querySQL(`INSERT INTO PURCHASE VALUES(${new_id}, DEFAULT, DEFAULT);`);
+				await main.querySQL(`INSERT INTO PURCHASE VALUES(${new_id}, ${section_select_supplier.dataset.idSupplier}, DEFAULT, DEFAULT);`);
 				for (const pi of current_products_items) {
 					await main.querySQL(`INSERT INTO PURCHASE_DETAIL VALUES(
 						(SELECT MAX(ID_PURCHASE_DETAIL) FROM PURCHASE_DETAIL) + 1,
@@ -189,7 +170,7 @@ async function MAIN(): Promise<void>
 			}
 
 
-		})
+		});
 
 	}
 	else if (main.aux.action == 'm')
@@ -201,11 +182,62 @@ async function MAIN(): Promise<void>
 		let purchase_header: any = (await main.querySQL(`SELECT * FROM PURCHASE WHERE ID_PURCHASE = ${main.aux.id};`)).rows[0];
 		let purchase_details: any[] = (await main.querySQL(`SELECT * FROM PURCHASE_DETAIL WHERE FK_PURCHASE = ${main.aux.id};`)).rows;
 
+		// Set ID
+		section_select_supplier.dataset.idSupplier = purchase_header.fk_supplier;
+		selected_supplier.value = (await main.querySQL(`SELECT NAME FROM SUPPLIER WHERE ID_SUPPLIER = ${purchase_header.fk_supplier};`)).rows[0].name;
+		id_purchase.value = purchase_header.fk_supplier;
+
+		// Fill info
+		for (const pd of purchase_details)
+			await addProductItem(pd.fk_product, pd.amount, pd.cost);
+
 		// Accept button
 		button_accept.addEventListener('click', async (): Promise<void> => {
-		
-		});
 
+			try
+			{
+				await validateinputs();
+				let current_products_items = document.getElementsByClassName('product_item') as HTMLCollectionOf<HTMLDivElement>;
+				// DB Query
+
+				await main.querySQL(`UPDATE PURCHASE SET STATUS = '${status.value}' WHERE ID_PURCHASE = ${purchase_header.id_purchase};`);
+				// Actualizar la fecha si dejamos el status en 'pendiente'
+				if (status.selectedIndex == 0)
+					await main.querySQL(`UPDATE PURCHASE SET DATE = NOW() WHERE ID_PURCHASE = ${purchase_header.id_purchase};`);
+
+				await main.querySQL(`DELETE FROM PURCHASE_DETAIL WHERE FK_PURCHASE = ${purchase_header.id_purchase};`);
+
+				for (const pi of current_products_items) {
+					await main.querySQL(`INSERT INTO PURCHASE_DETAIL VALUES(
+						(SELECT MAX(ID_PURCHASE_DETAIL) FROM PURCHASE_DETAIL) + 1,
+						${purchase_header.id_purchase},
+						${pi.dataset.id},
+						${(pi.querySelector('.amount') as HTMLInputElement).valueAsNumber},
+						${(pi.querySelector('.cost') as HTMLInputElement).valueAsNumber}
+					);`);
+				}
+
+				// Si cambiamos el estatus a 'entregado'
+				// Aumentar el stock general
+				if (status.selectedIndex == 1)
+				{
+					for (const pi of current_products_items) {
+						await main.querySQL(`UPDATE PRODUCT
+						SET STOCK = ((SELECT STOCK FROM PRODUCT WHERE ID_PRODUCT = ${pi.dataset.id}) + (${(pi.querySelector('.amount') as HTMLInputElement).valueAsNumber}))
+						WHERE ID_PRODUCT = ${pi.dataset.id};`);
+					}
+				}
+
+				dialog.showMessageBoxSync(getCurrentWindow(), {title: "Éxito", message: "Registro exitoso", type: "info"});
+				getCurrentWindow().close();
+			}
+			catch (error: any)
+			{
+				dialog.showMessageBoxSync(getCurrentWindow(), {title: "Error", message: error.message, type: "error"});
+			}
+
+
+		});
 	}
 }
 MAIN();
@@ -243,3 +275,21 @@ async function addProductItem(id: number, amount: number, cost: number)
 	product_item_list.appendChild(product_item);
 }
 
+
+async function validateinputs(): Promise<void>
+{
+	// Check at least 1 added element
+	let current_products_items = document.getElementsByClassName('product_item') as HTMLCollectionOf<HTMLDivElement>;
+	if (current_products_items.length == 0)
+		throw {message: "Ingrese por lo menos 1 producto a la compra"};
+
+	// Verificar que no rebase el stock maximo
+	for (const pi of current_products_items)
+	{
+		let product_info: any = (await main.querySQL(`SELECT MAX_STOCK, STOCK FROM PRODUCT WHERE ID_PRODUCT = ${parseInt(pi.dataset.id)};`)).rows[0];
+		let current_stock: number = parseInt(product_info.stock);
+		let max_stock: number = parseInt(product_info.max_stock);
+		if ((current_stock + (pi.querySelector('.amount') as HTMLInputElement).valueAsNumber) > max_stock)
+			throw {message: `La cantidad del producto '${pi.querySelector('.name').innerHTML}' excede el stock maximo (${max_stock})`};
+	}
+}
